f3112300f7ab96e4b8f17aff2dee5cbe5556435f
XJTAN630 <xiaojia_tip@163.com>
2025-08-06 22:07:53 +0800

    后端功能完善

---
+++ oj-backend/oj-backend-common/src/main/java/com/yupi/yuojbackendcommon/constant/FileConstant.java
@@ -0,0 +1,15 @@
+package com.xj.ojbackendcommon.constant;
+
+/**
+ * 文件常量
+ *
+
+ */
+public interface FileConstant {
+
+    /**
+     * COS 访问地址
+     * todo 需替换配置
+     */
+    String COS_HOST = "https://github.com/XJTAN630";
+}
